55cafe0cfde7285d1e4b8746053d3b84b143dd08
Umaha Tokula <umahatokula@gmail.com>
2024-03-17 10:35:26 +0100

    What is red

--- components/WhatIsRed.tsx
+++ components/WhatIsRed.tsx
@@ -4,15 +4,20 @@ import Carousel from "./Carousel";
 
 const images: string[] = [
   "slide1.svg",
-  "slide1.svg",
-  "slide1.svg",
-  "slide1.svg",
+  "/RED+1.jpg",
+  "/RED+2.jpg",
+  "/RED+3.jpg",
+  "/RED+4.jpg",
+  "/RED+5.jpg",
+  "/RED+6.jpg",
+  "/RED+7.jpg",
+  "/RED+8.jpg",
 ];
 
 function WhatIsRed() {
   return (
     <div className="">
-      <h3 className="text-red font-semibold text-base  leading-4 mx-5 mb-5 mt-10">
+      <h3 className="mx-5 mt-10 mb-5 text-base font-semibold leading-4 text-red">
         What is RED?
       </h3>
       <Carousel imagePaths={images} isFullWidth={true} />
